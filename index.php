be98f5c6a8db9735c50a336b0a9a3550b57f6409
Tommi Alajoki <tommi@alajoki.fi>
2018-01-03 22:48:35 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<form action="sent.php" method="post">
+Sender: <input type="text" name="sender"><br>
+Receiver: <input type="text" name="receiver"><br>
+
+Text: <textarea name="text" rows="50" cols="200"></textarea><br>
+
+<input type="submit">
---
+++ sent.php
@@ -0,0 +1,77 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'values.php';
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+$mail = new PHPMailer(true);
+$values = new Values(); // I should probably make these inputtable instead of static
+
+$sender = $_POST["sender"];
+$receiver = $_POST["receiver"];
+$text = $_POST["text"];
+
+$finalText = '';
+
+for ($i = 0; $i < strlen($text); $i++){
+    
+    if($text[$i] == "\n"){                                // Replaces newline chars with html breaks
+        $finalText .= '<br>';                               // for mailing as HTML
+    } else {
+        $finalText .= $text[$i];
+    }
+}
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                                 // Enable verbose debug output
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = $values->smtp;                       // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = $values->username;                 // SMTP username
+    $mail->Password = $values->pw;                        // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;                                    // TCP port to connect to
+    
+    $mail->CharSet = 'utf-8';
+
+    //Recipients
+    $mail->setFrom($sender);
+    $mail->addAddress($receiver);     // Add a recipient
+//    $mail->addAddress('ellen@example.com');               // Name is optional
+//    $mail->addReplyTo('info@example.com', 'Information');
+//    $mail->addCC('cc@example.com');
+//    $mail->addBCC('bcc@example.com');
+
+    //Attachments
+//    $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+    //Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'test';
+    $mail->Body    = $finalText;
+    $mail->AltBody = $finalText;
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+}
+
+
+
+?>
+
+<form action="index.php" method="post">
+    <input type="submit" name="Back" value="Back" />
+</form>
+
+
+
