ce0ea00f15136dbf41ca6c13a0acb74d4b9c222f
Tommi Alajoki <tommi@alajoki.fi>
2018-01-07 21:25:10 +0200

    Same

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <form action="sent.php" method="post">
 Sender: <input type="text" name="sender"><br>
 Receiver: <input type="text" name="receiver"><br>
+Subject: <input type="text" name="subject"><br>
 
 Text: <textarea name="text" rows="50" cols="200"></textarea><br>
 
