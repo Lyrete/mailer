73914ae0f2fbd478329d14c2acdc887e987e3045
Tommi Alajoki <tommi@alajoki.fi>
2018-01-07 21:55:15 +0200

    Initial commit for functionality to input events for constructing the newsletter.

---
+++ wnlAdd.php
@@ -0,0 +1,8 @@
+<form action="wnlAdded.php" method="post">
+    Event: <input type="text" name="subject"><br>
+    Start date: <input type="date" name="startDate"> End date: <input type="Date" name="endDate">
+    <br>
+
+    Text: <textarea name="text" rows="30" cols="150"></textarea><br>
+
+<input type="submit">
---
+++ wnlAdded.php
@@ -0,0 +1,23 @@
+<?php
+
+$text = $_POST["text"];
+
+$finalText = '';
+
+for ($i = 0; $i < strlen($text); $i++){
+    
+    if($text[$i] == "\n"){                                // Replaces newline chars with html breaks
+        $finalText .= '<br>';                               // for mailing as HTML
+    } else {
+        $finalText .= $text[$i];
+    }
+}
+
+?>
+
+
+
+<form action="wnlAdd.php" method="post">
+    Added.<br>
+<input type="submit" value="Back">
+
