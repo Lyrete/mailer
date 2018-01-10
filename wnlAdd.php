8ffc363be6836b08f9d8cbe602cc10ab00d7f1ac
Tommi Alajoki <tommi@alajoki.fi>
2018-01-10 22:58:07 +0200

    Modified a bit.

--- wnlAdd.php
+++ wnlAdd.php
@@ -1,11 +1,8 @@
 <form action="wnlAdded.php" method="post">
     Event: <input type="text" name="subject"><br>
-    Start date: <input type="date" name="startDate"> End date: <input type="Date" name="endDate">
-    <br>
-    <br>
-    <?php
-    echo microtime(true)
-    ?><br>
+    Start date: <input type="date" name="startDate"><br>
+    End date: <input type="Date" name="endDate"> (Don't enter if the event is one day only)
+    <br>    
     <br>
     Text: <textarea name="text" rows="30" cols="150"></textarea><br>
 
