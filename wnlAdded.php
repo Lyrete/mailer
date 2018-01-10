13b46d4d9a531cc565e992dd128344c79ec2355d
Tommi Alajoki <tommi@alajoki.fi>
2018-01-11 00:51:42 +0200

    More TG functionality plus date to subject.

--- wnlAdded.php
+++ wnlAdded.php
@@ -1,5 +1,7 @@
 <?php
 
+require 'Date.php';
+
 $text = $_POST["text"];
 
 $finalText = '';
@@ -13,19 +15,30 @@ for ($i = 0; $i < strlen($text); $i++){
     }
 }
 
-//$textForBot = [];
-//
-//if(strlen($text) > 4096){
-//    $counter = strlen($text);
-//    for($i = 0; $counter > 4096; $counter - 4096){
-//        $textForBot[$i] = substr($text, $i * 4096, $i * 4096 + 4096);  
-//    }
-//    $textForBot[$i] = substr($text, $i * 4096);
-//} else {
-//    $textForBot[$i] = $text;
-//}
+$start = $_POST["startDate"];
+$end = $_POST["endDate"];
 
+$day = substr($start, 8, 2);
+$month = substr($start, 5, 2);
 
+$startDate = new Date($day, $month);
+
+$day = substr($end, 8, 2);
+$month = substr($end, 5, 2);
+
+$endDate = new Date($day, $month);
+
+if($endDate->day == '' and $endDate->month == ''){
+    $duration = (string)$startDate;
+} else {
+    $duration = $startDate . '-' . $endDate;
+}
+
+$subject = $_POST["subject"] . ' ' . $duration;
+
+$text = '<b>' . $subject . '</b>' . "\n" . "\n" . $text;
+
+//TG message functionality
 
 $channel = "-1001398336786";
 $api_token = "416904756:AAGGqOLio1vAS2KpbqqMKiZKmFk_pJVm3nU";
@@ -34,18 +47,16 @@ $url = "https://api.telegram.org/bot";
 
 //add options to url
 
-$url = $url . $api_token . $method . '?chat_id=' . $channel . '&text=';
+$url = $url . $api_token . $method . '?chat_id=' . $channel . '&parse_mode=HTML' . '&text=';
 
 if(strlen($text) > 4096){
     $arr1 = str_split($text, 4096);
     foreach($arr1 as $part){
-        $ch = curl_init($url . $part);
+        $ch = curl_init($url . urlencode($part));
         curl_exec($ch);
     }
 } else {
-    echo $text;
-    echo $url . $text;
-    $ch = curl_init($url . $text);
+    $ch = curl_init($url . urlencode($text));
     curl_exec($ch);
 }
         
