b36e87a9452e620129aee4e20846d34676a6c0cb
Tommi <tommi@alajoki.fi>
2018-01-22 19:39:15 +0200

    Initial commit.

---
+++ eventIndex.php
@@ -0,0 +1,10 @@
+<?php
+
+?>
+
+<form action="wnlAdd.php" method="post">
+    DB-user: <input type="text" name="user"> (<b>Don't enter a date at the end the script does it automatically!!!!!</b>)<br>
+    Password: <input type="password" name="pw"><br>
+
+<input type="submit">
+
