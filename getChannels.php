5889c1c182cff1ca8a6040d67ab1416c572a503a
Tommi <tommi@alajoki.fi>
2018-01-30 01:45:32 +0200

    Working channel search, getUpdates is broken?

--- getChannels.php
+++ getChannels.php
@@ -16,11 +16,12 @@ $db = new DB();
 
 $api_token = $db->getBotApi();
 $method = "/getUpdates";
-$url = "https://api.telegram.org/bot";
+$urlStart = "https://api.telegram.org/bot";
 
 //add options to url
 
-$url = $url . $api_token . $method . '?offset=184451479';
+$urlStart .= $api_token; 
+$url = $urlStart . $method . '?offset=-25';
 
 echo $url . '<br><br>';
 
@@ -28,19 +29,30 @@ $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $result = curl_exec($ch);
 
-$arrayString = substr($result,21, -2);
-
-echo $arrayString . '<br><br>';
-
-$test = str_ireplace("},{", '}splitstrhere{', $arrayString);
-
-//echo $test . '<br><br>';
-
-$array = explode('splitstrhere',$test);
+echo $result . '<br><br>';
+$jsonObject = json_decode($result);
+$array = $jsonObject->result;
 
 foreach($array as $a){
-    echo $a . '<br><br>';
+    $channel = $a->message->chat->id;
+    $name = $a->message->chat->title;
+    if($db->insertIfNotIn($channel, $name)){
+        //If channel wasn't in db send welcome msg.
+        $message = "Added to database!\nWelcome!";
+        $method = '/sendMessage';
+        $url = $urlStart . $method . '?chat_id=' . $channel . '&text=' . urlencode($message);
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $result = curl_exec($ch);
+    }else{
+        $message = "Was already in database boo!\nWelcome!";
+        $method = '/sendMessage';
+        $url = $urlStart . $method . '?chat_id=' . $channel . '&text=' . urlencode($message);
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $result = curl_exec($ch);
+        
+    }
+    
+    echo '<br>';
 }
-
-
-
