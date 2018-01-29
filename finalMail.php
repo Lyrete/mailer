977c9563ae9a68d28f77431ceb4aca373fd57bf7
Tommi <tommi@alajoki.fi>
2018-01-29 02:02:50 +0200

    Write to file added. Attachments to zip added. Pushes the contents to DB.

--- finalMail.php
+++ finalMail.php
@@ -1,11 +1,14 @@
 <?php
 
+include 'database.php';
+
 session_start();
 
 if($_SESSION["user"] != NULL){
     
     include 'navigation.php';
-
+    
+    $db = new DB();
 ?>
 
 
@@ -14,6 +17,21 @@ if($_SESSION["user"] != NULL){
     Receiver: <input type="text" name="receiver"><br>
     Subject: <input style="width:400px" type="text" name="subject"><br>
     When to send: <input type="time" name="timeToSend" id="timeToSend"><br>
+    Which week to construct for: <select name="week">
+    <?php
+            for ( $i == 1; $i <= date('W',strtotime('December 24')) ; $i += 1 ){
+                echo '<option value="';
+                echo $i;
+                if($i == (int)date('W', strtotime('today'))){
+                    echo '" selected>';
+                } else {
+                    echo '">';
+                }
+                echo $i;
+                echo '</option>';
+            }    
+    ?>
+    </select>
 
     <br>
 
@@ -28,8 +46,39 @@ if($_SESSION["user"] != NULL){
 if(isset($_POST["sender"])==TRUE){
     
     include 'construct.php';
+       
+    $filename = 'WNL/wnl_' . $_POST["week"] . '.html';
     
-    include 'mailit.php';
+    $file = fopen($filename, 'w');
+    
+    chmod($filename, 0775);
+    
+    fwrite($file, nl2br($mailtext));
+    
+    fclose($file);
+    
+    $zippath = 'WNL/attachments/WNL' . $_POST["week"] . '.zip';  
+    
+    echo $zippath;
+    
+    $zip = new ZipArchive();
+       
+    if ($zip->open($zippath, ZIPARCHIVE::CREATE | ZIPARCHIVE::OVERWRITE) === TRUE){
+        echo 'are we here?';
+        // Add files to the zip file
+        for($i=0; $i < sizeof($attachments); $i++){ 
+            echo 'this too?';
+            $zip->addFile(substr($attachments[$i],1), substr($attachments[$i],13));         // Add attachments to zip            
+        }      
+        // All files are added, so close the zip file.
+        $zip->close();
+    }
+    
+    chmod($zippath, 0775);
+    
+    echo $db->addFullLetter($mailtext, $week, $filename, $zippath);
+    
+//    include 'mailit.php';
 }
 
 } else {
