36391b917b500fd4e5bcbd3e92e885ecefc552aa
Tommi Alajoki <tommi@alajoki.fi>
2018-09-02 18:03:21 +0300

    Required tags to some.

--- wnlAdd.php
+++ wnlAdd.php
@@ -10,7 +10,7 @@ if($_SESSION["user"] != NULL){
 
 
 <form action="wnlAdded.php" method="post" accept-charset="utf-8">
-    Event: <input type="text" name="subject"> (<b>Don't enter a date at the end the script does it automatically!!!!!</b>)<br>
+    Event: <input type="text" name="subject" required> (<b>Don't enter a date at the end the script does it automatically!!!!!</b>)<br>
     Start date: <input type="date" name="startDate"><br>
     End date: <input type="Date" name="endDate"> (Don't enter if the event is one day only)
     <br>
@@ -18,7 +18,7 @@ if($_SESSION["user"] != NULL){
     <input type="radio" name="eventType" value="muu">Muu tapahtuma
     <input type="radio" name="eventType" value="ylim">Muuta
     <br>
-    Text: <textarea name="text" rows="30" cols="150"></textarea><br>
+    Text: <textarea name="text" rows="30" cols="150" required></textarea><br>
 
 <input type="submit">
 
