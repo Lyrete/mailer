d7e7dccbef69a57c8913b580fd37698be099b481
Tommi Alajoki <tommi@alajoki.fi>
2018-11-19 03:04:32 +0200

    Changed the recognition of ongoing events.

--- construct.php
+++ construct.php
@@ -25,7 +25,7 @@ $deadline->setISODate(date('Y',strtotime('today')), $week);
 $deadline->add(date_interval_create_from_date_string('200 days')); //make this back into 28 days later.
 
 foreach ($wholeresult as $row){
-    if($row["startDate"] >= date('Y-m-d') and strtotime($row["startDate"]) < strtotime($deadline->format('Y-m-d'))){
+    if(($row["startDate"] >= date('Y-m-d') and strtotime($row["startDate"]) < strtotime($deadline->format('Y-m-d'))) or ($row["endDate"] != NULL and $row["endDate"] >= date("Y-m-d") and $row["startDate"] < date("Y-m-d"))){
         $date = date('j.n.',strtotime($row["startDate"]));
         
         if ( $row["endDate"] != '0000-00-00' and $row["endDate"] != NULL){
