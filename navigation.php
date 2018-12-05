e941a113b6374718e8e6dc67b46084387470f27f
Tommi Alajoki <tommi@alajoki.fi>
2018-12-06 01:05:06 +0200

    hid tg msg tab

--- navigation.php
+++ navigation.php
@@ -18,7 +18,7 @@ require_once 'database.php'
 <div class="links">
     <a href ="wnlAdd.php"><div class="pagePart">Add events</div></a>
     <a href ="finalMail.php"><div class="pagePart">Construct the newsletter</div></a>
-    <a href="TGmsg.php"><div class="pagePart">Send a TG message</div></a>
+<!--    <a href="TGmsg.php"><div class="pagePart">Send a TG message</div></a>-->
     <a href="addUser.php"><div class="pagePart">User management</div></a>
     <a href="addHeader.php"><div class="pagePart">Header insertion</div></a>
 </div>
