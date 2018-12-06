8c51e3e28f65984349819ca5e47408b4aa7744fe
Tommi Alajoki <tommi@alajoki.fi>
2018-12-06 16:25:51 +0200

    Initial commit of single event page.

---
+++ eventManager.php
@@ -0,0 +1,66 @@
+<?php
+include_once "userPDO.php";
+session_start();
+
+if($_SESSION["user"] != NULL){
+
+    include 'navigation.php';
+    $db = new DB();
+    include_once 'eventFetcher.php';
+
+    $eventFetcher = new eventFetcher();
+
+    $event = $eventFetcher->getEvent($_GET["id"]);
+
+    $types = array("kilta", "muu", "ylim");
+
+    ?>
+
+    <form>
+      <table>
+      <tr>
+        <td>Nimi</td>
+        <td><input type=text name="name" value="<?php echo $event->getName()?>"></td>
+      </tr>
+      <tr>
+        <td>Start Date</td>
+        <td><input type=Date name="startDate" value="<?php echo $event->getStartDate() ?>"></td>
+      </tr>
+      <tr>
+        <td>End Date</td>
+        <td><input type=Date name="endDate" value="<?php echo $event->getEndDate() ?>"></td>
+      </tr>
+      <tr>
+        <td>Type</td>
+        <td><select name="kategoria">
+          <?php
+            foreach($types as $type){
+              echo "<option value='" . $type . "' ";
+              if($type == $event->getKategoria()){
+                echo "selected";
+              }
+              echo ">" . $type . "</option>";
+            }
+           ?>
+
+        </select></td>
+      </tr>
+      <tr>
+        <td>Description</td>
+        <td><textarea name="text" rows="30" cols="100"><?php echo $event->getDescription() ?></textarea></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td><input type=submit value="Save"></td>
+      </tr>
+    </table>
+
+    </form>
+
+
+
+    <?php
+
+    } else {
+        include 'index.php';
+    } ?>
