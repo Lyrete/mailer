cda6ba451037d7cb278afdd2454ce35428b92199
Tommi Alajoki <tommi@alajoki.fi>
2018-12-06 02:31:20 +0200

    add eventmanager tab, switch order hide user management from non admin

--- navigation.php
+++ navigation.php
@@ -17,8 +17,11 @@ Logged in as <?php echo "<br>" . $user->getName() . " <br>" . $user->getEmail()?
 
 <div class="links">
     <a href ="wnlAdd.php"><div class="pagePart">Add events</div></a>
-    <a href ="finalMail.php"><div class="pagePart">Construct the newsletter</div></a>
+    <a href ="eventManager.php"><div class="pagePart">Manage events</div></a>
 <!--    <a href="TGmsg.php"><div class="pagePart">Send a TG message</div></a>-->
+<?php if($user->getUsr_lvl() == "admin"){?>
     <a href="addUser.php"><div class="pagePart">User management</div></a>
+<?php }?>
     <a href="addHeader.php"><div class="pagePart">Header insertion</div></a>
+    <a href ="finalMail.php"><div class="pagePart">Construct the newsletter</div></a>
 </div>
