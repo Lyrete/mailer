9cad25b613826e72b481bce0dda5ab768b68fcb1
Tommi Alajoki <tommi@alajoki.fi>
2018-12-06 15:32:23 +0200

    renamed manager to replace old db-index

--- eventIndex.php
+++ eventIndex.php
@@ -1,42 +1,42 @@
-<form action="eventIndex.php" method="post">
-    DB-user: <input type="text" name="user"><br>
-    Password: <input type="password" name="pw"><br>   
+<?php
+include_once "userPDO.php";
+session_start();
 
-    <input type="submit"> <br> <br>
-    
-</form>
+if($_SESSION["user"] != NULL){
+
+    include 'navigation.php';
+    $db = new DB();
+    include_once 'eventFetcher.php';
+
+    $eventFetcher = new eventFetcher();
+
+    $events = $eventFetcher->getAllEvents("date");
+
+    ?>
+
+  <table>
+    <tr>
+      <th>StartDate</th>
+      <th>Name</th>
+    </tr>
+
+    <?php
+
+    foreach ($events as $event){
+        echo "<tr><td>";
+        echo $event->getStartDate() . "</td><td>";
+        echo $event->getName();
+        echo "</td></tr>";
+    }
+
+    ?>
+
+  </table>
+
+
 
 <?php
-    if (isset($_POST["user"]) == TRUE){
-        $servername = "localhost";
-        $username = $_POST["user"];
-        $password = $_POST["pw"];
-        $dbName = 'newsletter';
-        
-        // Create connection
-        $conn = mysqli_connect($servername, $username, $password, $dbName);
 
-        // Check connection
-        if (!$conn) {
-            die("Connection failed: " . mysqli_connect_error());
-        }
-        echo "Connected successfully";
-        
-        session_start();
-        
-        $_SESSION["db"] = $conn;
-        
-        ?>
-    <br>
-        <form action="wnlAdd.php">
-            <input type="submit" value="Start adding events.">
-            
-        </form>
-    
-        <?php
-        
-        }else{
-            echo 'Enter user and password.';
-        }
-
-?>
+} else {
+    include 'index.php';
+} ?>
