6daa590a1245f5b81ab4adf8a697b0a3d49c4d19
Tommi Alajoki <tommi@alajoki.fi>
2019-01-03 13:53:02 +0200

    get the user and check for him, display info above navbar

--- navigation.php
+++ navigation.php
@@ -3,7 +3,11 @@ require_once 'database.php';
 require_once 'userPDO.php';
 require_once 'user.php';
 
-$user = $_SESSION["user"]
+if(isset($_SESSION["user"])){
+  $user = $_SESSION["user"];
+}else{
+  $user = NULL;
+}
 
 ?>
 
@@ -13,7 +17,9 @@ $user = $_SESSION["user"]
     <title>Newsletter CP</title>
 </head>
 
-Logged in as <?php echo "<br>" . $user->getName() . " <br>" . $user->getEmail()?><br><br>
+<?php if(isset($user)){?>
+  Logged in as <?php echo "<br>" . $user->getName() . " <br>" . $user->getEmail()?><br><br>
+
 
 <div class="links">
     <a href ="wnlAdd.php"><div class="pagePart">Add events</div></a>
@@ -25,3 +31,5 @@ Logged in as <?php echo "<br>" . $user->getName() . " <br>" . $user->getEmail()?
     <a href="addHeader.php"><div class="pagePart">Header insertion</div></a>
     <a href ="finalMail.php"><div class="pagePart">Construct the newsletter</div></a>
 </div>
+
+<?php }?>
